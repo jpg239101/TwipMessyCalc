10c4aa7bf4e96fc540dffe36efa4c224181ae0bd
jg101 <jpg239101@gmail.com>
2016-09-20 14:17:54 -0400

    for change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,9 @@ public class Main {
         boolean nonBreakable = true;
         String num1;
         String num2;
+        boolean yes = true;
+        double val1 = 0;
+        double val2 = 0;
 
         while(nonBreakable) {
             System.out.println("Please select an operation 'add' 'subtract' 'multiply' 'divide'");
@@ -20,34 +23,51 @@ public class Main {
             System.out.println("Input your second number");
             num2 = kb.next();
 
-            double val1 = Double.parseDouble(num1);
-            double val2 = Double.parseDouble(num2);
+            if (num1.contains("0") || num1.contains("1") || num1.contains("2") || num1.contains("3") || num1.contains("4") || num1.contains("5") || num1.contains("6") || num1.contains("7") || num1.contains("8") || num1.contains("9")){
+                val1 = Double.parseDouble(num1);
+            }
+            else {
+                System.out.println("please use a real number for the first #");
+                yes = false;
+            }
+            if (num2.contains("0") || num2.contains("1") || num2.contains("2") || num2.contains("3") || num2.contains("4") || num2.contains("5") || num2.contains("6") || num2.contains("7") || num2.contains("8") || num2.contains("9")) {
+                val2 = Double.parseDouble(num2);
+            }
+            else{
+                System.out.println("please use a real number for the second #");
+                yes = false;
+            }
 
 
-            if (operator.equals("add")){
+            if (operator.equals("add") && yes){
                 Double sum = val1 + val2;
                 System.out.println(sum);
                 break;
             }
 
-            else if (operator.equals("subtract")){
+            else if (operator.equals("subtract") && yes){
                 double sum = val1 - val2;
                 System.out.println(sum);
                 break;
             }
 
-            else  if (operator.equals("multiply")){
+            else  if (operator.equals("multiply") && yes){
                 double sum = val1 * val2;
                 System.out.println(sum);
+                break;
             }
 
-            else if (operator.equals("divide")){
+            else if (operator.equals("divide") && yes){
                 double sum = val1 / val2;
                 System.out.println(sum);
+                break;
             }
 
-            else{
+            else if (yes){
                 System.out.println("Please select from the list");
+            }
+            else{
+
             }
         }
 
